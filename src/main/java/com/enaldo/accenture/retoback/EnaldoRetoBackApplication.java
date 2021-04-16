09445ee1af320c67d19d3bc2b0c6ec85876d5622
Enaldo Narvaez <enaldo_17@hotmail.com>
2021-04-16 16:23:29 -0500

    Creacion del proyecto
    Creación y configuración inicial del proyecto usando el asistente de Spring Boot.

---
+++ src/main/java/com/enaldo/accenture/retoback/EnaldoRetoBackApplication.java
@@ -0,0 +1,13 @@
+package com.enaldo.accenture.retoback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnaldoRetoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnaldoRetoBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enaldo/accenture/retoback/SqLiteDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enaldo.accenture.retoback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqLiteDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
