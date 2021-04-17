f4e0aec384504aeb47c902377e70307920408191
Enaldo Narvaez <enaldo_17@hotmail.com>
2021-04-17 16:47:25 -0500

    Clase Respuesta
    Se crea una clase de respuesta para facilitar el retorno datos, tambien se configura el servicio para poder usar esta clase.

---
+++ src/main/java/com/enaldo/accenture/retoback/model/RepuestaModelo.java
@@ -0,0 +1,39 @@
+package com.enaldo.accenture.retoback.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RepuestaModelo {
+	private String mensaje;
+	private List<FacturaModelo> facturas =new ArrayList<FacturaModelo>();;
+	
+	public RepuestaModelo() {
+		
+	}
+	public RepuestaModelo(String mensaje, FacturaModelo facturas) {
+		this.mensaje = mensaje;
+		this.facturas.add(facturas);
+	}
+	public RepuestaModelo(String mensaje, List<FacturaModelo> facturas) {
+		this.mensaje = mensaje;
+		this.facturas.addAll(facturas);
+	}
+	
+	
+	public String getMensaje() {
+		return mensaje;
+	}
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+	public List<FacturaModelo> getFacturas() {
+		return facturas;
+	}
+	public void setFacturas(List<FacturaModelo> facturas) {
+		this.facturas.addAll(facturas);
+	}
+	
+	
+	
+
+}
